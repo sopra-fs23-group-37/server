799cd179100aac518f4794ce23421fd0e23a922e
niezwe <niels.zweifel@uzh.ch>
2023-04-04 15:10:35 +0200

    added test cases for CardDeckService

--- src/main/java/ch/uzh/ifi/hase/soprafs23/service/CardDeckService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/service/CardDeckService.java
@@ -13,30 +13,29 @@ import ch.uzh.ifi.hase.soprafs23.entity.CardDeck;
 import ch.uzh.ifi.hase.soprafs23.entity.CardDrawResponse;
 
 public class CardDeckService {
+	HttpClient httpClient = HttpClient.newHttpClient();
+	ObjectMapper objectMapper = new ObjectMapper();
+
     public CardDeck createDeck() throws IOException, InterruptedException {
         String uri = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
 		// build get request and get deck id
-		HttpClient httpClient = HttpClient.newHttpClient();
+		
 
 		HttpRequest request = HttpRequest.newBuilder()
 			.GET()
 			.uri(URI.create(uri))
 			.build();
-		
+			
 		HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 
-		System.out.println(response.body());
-		ObjectMapper objectMapper = new ObjectMapper();
-
 		CardDeck newDeck = objectMapper.readValue(response.body(), CardDeck.class);
-		System.out.println(newDeck.getDeck_id());
+
         return newDeck;
     }
 
     public Card[] drawCards(CardDeck deck, int number) throws IOException, InterruptedException {
         String uri = String.format("https://deckofcardsapi.com/api/deck/%s/draw/?count=%s", deck.getDeck_id(), number);
 		// build get request and get deck id
-		HttpClient httpClient = HttpClient.newHttpClient();
 
 		HttpRequest request = HttpRequest.newBuilder()
 			.GET()
@@ -45,7 +44,6 @@ public class CardDeckService {
 		
 		HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 
-		ObjectMapper objectMapper = new ObjectMapper();
 		CardDrawResponse cardDrawResponse = objectMapper.readValue(response.body(), CardDrawResponse.class);
 
         return cardDrawResponse.getCards();
@@ -54,7 +52,6 @@ public class CardDeckService {
 	public CardDeck shuffleDeck(CardDeck deck) throws IOException, InterruptedException {
 		String uri = String.format("https://deckofcardsapi.com/api/deck/%s/shuffle/", deck.getDeck_id());
 		// build get request and get deck id
-		HttpClient httpClient = HttpClient.newHttpClient();
 
 		HttpRequest request = HttpRequest.newBuilder()
 			.GET()
@@ -63,7 +60,6 @@ public class CardDeckService {
 		
 		HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 
-		ObjectMapper objectMapper = new ObjectMapper();
 		CardDeck deckResponse = objectMapper.readValue(response.body(), CardDeck.class);
 		
         return deckResponse;
