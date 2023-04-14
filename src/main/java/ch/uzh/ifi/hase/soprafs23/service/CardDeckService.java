7ad26a3bb86bee4d3b0b3561c72ed3ae7914c6b6
niezwe <niels.zweifel@uzh.ch>
2023-04-14 12:28:23 +0200

    small deletions

--- src/main/java/ch/uzh/ifi/hase/soprafs23/service/CardDeckService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/service/CardDeckService.java
@@ -6,7 +6,6 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
--- src/test/java/ch/uzh/ifi/hase/soprafs23/service/MoveLogicServiceTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/service/MoveLogicServiceTest.java
@@ -2,7 +2,6 @@ package ch.uzh.ifi.hase.soprafs23.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.beans.Transient;
 import java.util.ArrayList;
 import java.util.List;
 
