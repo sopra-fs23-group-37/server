0118980010fef5a2099bb27a4ee6ce3d6fedb62b
trofej <timon.fopp@uzh.ch>
2023-04-19 15:17:05 +0200

    Implemented Database specs, application.properties

--- src/main/java/ch/uzh/ifi/hase/soprafs23/entity/User.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/entity/User.java
@@ -17,7 +17,7 @@ import java.util.Date;
  * the primary key
  */
 @Entity
-@Table(name = "USER")
+@Table(name = "user")
 public class User implements Serializable {
 
   private static final long serialVersionUID = 1L;
