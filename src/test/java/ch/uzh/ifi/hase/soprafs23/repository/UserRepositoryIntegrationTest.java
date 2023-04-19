ddeb97b738ea274393f40578721b27d4a83a3907
trofej <timon.fopp@uzh.ch>
2023-04-19 17:36:32 +0200

    UserRepositoryIntegrationTest uncommented, works now

--- src/test/java/ch/uzh/ifi/hase/soprafs23/repository/UserRepositoryIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/repository/UserRepositoryIntegrationTest.java
@@ -1,62 +1,62 @@
-// package ch.uzh.ifi.hase.soprafs23.repository;
-// import ch.uzh.ifi.hase.soprafs23.constant.UserStatus;
-// import ch.uzh.ifi.hase.soprafs23.entity.User;
-// import org.junit.jupiter.api.BeforeEach;
-// import org.junit.jupiter.api.Test;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-// import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+package ch.uzh.ifi.hase.soprafs23.repository;
+import ch.uzh.ifi.hase.soprafs23.constant.UserStatus;
+import ch.uzh.ifi.hase.soprafs23.entity.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
-// import java.util.Date;
+import java.util.Date;
 
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-// import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-// @DataJpaTest
-// public class UserRepositoryIntegrationTest {
+@DataJpaTest
+public class UserRepositoryIntegrationTest {
 
-// @Autowired
-// private TestEntityManager entityManager;
+    @Autowired
+    private TestEntityManager entityManager;
 
-// @Autowired
-// private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-// private User user = new User();
+    private User user = new User();
 
-// @BeforeEach
-// private void given(){
-// user.setUsername("Max");
-// user.setPassword("123");
-// user.setUserStatus(UserStatus.ONLINE);
-// user.setToken("1");
-// user.setCreation_date(new Date());
+    @BeforeEach
+    private void given(){
+        user.setUsername("Max");
+        user.setPassword("123");
+        user.setUserStatus(UserStatus.ONLINE);
+        user.setToken("1");
+        user.setCreation_date(new Date());
 
-// entityManager.persistAndFlush(user);
-// }
+        entityManager.persistAndFlush(user);
+    }
 
-// @Test
-// public void findByUsername_success() {
-// // when
-// User found = userRepository.findByUsername(user.getUsername());
+    @Test
+    public void findByUsername_success() {
+        // when
+        User found = userRepository.findByUsername(user.getUsername());
 
-// // then
-// assertNotNull(found.getUserId());
-// assertEquals(found.getPassword(), user.getPassword());
-// assertEquals(found.getUsername(), user.getUsername());
-// assertEquals(found.getToken(), user.getToken());
-// assertEquals(found.getUserStatus(), user.getUserStatus());
-// }
+        // then
+        assertNotNull(found.getUserId());
+        assertEquals(found.getPassword(), user.getPassword());
+        assertEquals(found.getUsername(), user.getUsername());
+        assertEquals(found.getToken(), user.getToken());
+        assertEquals(found.getUserStatus(), user.getUserStatus());
+    }
 
-// @Test
-// public void findByUserId_success() {
-// // when
-// User found = userRepository.findByUserId(user.getUserId());
+    @Test
+    public void findByUserId_success() {
+        // when
+        User found = userRepository.findByUserId(user.getUserId());
 
-// // then
-// assertNotNull(found.getUserId());
-// assertEquals(found.getPassword(), user.getPassword());
-// assertEquals(found.getUsername(), user.getUsername());
-// assertEquals(found.getToken(), user.getToken());
-// assertEquals(found.getUserStatus(), user.getUserStatus());
-// }
-// }
+        // then
+        assertNotNull(found.getUserId());
+        assertEquals(found.getPassword(), user.getPassword());
+        assertEquals(found.getUsername(), user.getUsername());
+        assertEquals(found.getToken(), user.getToken());
+        assertEquals(found.getUserStatus(), user.getUserStatus());
+    }
+}
