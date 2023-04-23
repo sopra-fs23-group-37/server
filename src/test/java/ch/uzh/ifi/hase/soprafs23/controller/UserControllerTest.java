2f0540583b2003a1485f2675e5605f751859ec26
trofej <timon.fopp@uzh.ch>
2023-04-23 12:28:48 +0200

    Added some comments, adjusted some test (added setup() at beginning)

--- src/test/java/ch/uzh/ifi/hase/soprafs23/controller/UserControllerTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/controller/UserControllerTest.java
@@ -7,6 +7,7 @@ import ch.uzh.ifi.hase.soprafs23.rest.dto.UserPutDTO;
 import ch.uzh.ifi.hase.soprafs23.service.UserService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,19 +15,17 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.web.server.ResponseStatusException;
-
-
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
@@ -36,125 +35,111 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
-/**
- * UserControllerTest
- * This is a WebMvcTest which allows to test the UserController i.e. GET/POST
- * request without actually sending them over the network.
- * This tests if the UserController works.
- */
 @WebMvcTest(UserController.class)
 public class UserControllerTest {
 
-  @Autowired
-  private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-  @MockBean
-  private UserService userService;
+    @MockBean
+    private UserService userService;
 
-  // 1. There is one user in the list, because we created just one user, so 200 OK expected
-  @Test
-  public void givenUsers_whenGetUsers_thenReturnJsonArray() throws Exception {
-    // given
-    User user = new User();
-    user.setUsername("firstname@lastname");
-    user.setUserStatus(UserStatus.OFFLINE);
-    user.setPassword("password");
-    user.setCreation_date(new Date());
+    private User user;
+    private UserPostDTO userPostDTO;
 
-    List<User> allUsers = Collections.singletonList(user);
+    @MockBean
+    private UserPutDTO userPutDTO;
 
-    // this mocks the UserService -> we define above what the userService should
-    // return when getUsers() is called
-    given(userService.getUsers()).willReturn(allUsers);
 
-    // when
-    MockHttpServletRequestBuilder getRequest = get("/users").contentType(MediaType.APPLICATION_JSON);
+    @BeforeEach
+    void setup() {
+        user = new User();
+        user.setUserId(999L);
+        user.setUsername("firstname@lastname");
+        user.setUserStatus(UserStatus.OFFLINE);
+        user.setPassword("testPassword");
+        user.setCreation_date(new Date());
 
-    // then
-    mockMvc.perform(getRequest).andExpect(status().isOk())
-        .andExpect(jsonPath("$", hasSize(1)))
-        //.andExpect(jsonPath("$[0].name", is(user.getName())))
-        .andExpect(jsonPath("$[0].username", is(user.getUsername())))
-        .andExpect(jsonPath("$[0].userStatus", is(user.getUserStatus().toString())));
-  }
+        userPostDTO = new UserPostDTO();
+        userPostDTO.setUsername("testUsername");
+        userPostDTO.setPassword("testPassword");
 
-  // 2. Create user: Status Code 201 CREATED expected
-  @Test
-  public void createUser_validInput_userCreated() throws Exception {
-    // given
-    User user = new User();
-    user.setUserId(1L);
-    //user.setName("Test User");
-    user.setUsername("testUsername");
-    user.setToken("1");
-    user.setUserStatus(UserStatus.ONLINE);
-    user.setPassword("password");
-    user.setCreation_date(new Date());
+        userPutDTO = new UserPutDTO();
+        userPutDTO.setUsername("otherUsername");
+        userPutDTO.setBirthday(new Date(552218400000L));
 
-    UserPostDTO userPostDTO = new UserPostDTO();
-    //userPostDTO.setName("Test User");
-    userPostDTO.setUsername("testUsername");
-    userPostDTO.setPassword("password");
 
-    given(userService.createUser(Mockito.any())).willReturn(user);
+    }
 
-    // when/then -> do the request + validate the result
-    MockHttpServletRequestBuilder postRequest = post("/users")
-        .contentType(MediaType.APPLICATION_JSON)
-        .content(asJsonString(userPostDTO));
+    // 1. There is one user in the list, because we created just one user, so 200 OK expected
+    @Test
+    public void givenUsers_whenGetUsers_thenReturnJsonArray() throws Exception {
+        List<User> allUsers = Collections.singletonList(user);
+        given(userService.getUsers()).willReturn(allUsers);
 
-    // then
-    mockMvc.perform(postRequest)
-        .andExpect(status().isCreated())
-        .andExpect(jsonPath("$.userId", is(user.getUserId().intValue())))
-        //.andExpect(jsonPath("$.name", is(user.getName())))
-        .andExpect(jsonPath("$.username", is(user.getUsername())))
-        .andExpect(jsonPath("$.userStatus", is(user.getUserStatus().toString())));
-  }
+        MockHttpServletRequestBuilder getRequest = get("/users").contentType(MediaType.APPLICATION_JSON);
 
-  // 3. Create user: new user failed because username already exists: Status Code 409 CONFLICT expected
-  @Test
-  public void createUser_invalidInput() throws Exception {
-    given(userService.createUser(Mockito.any())).willThrow(new ResponseStatusException(HttpStatus.CONFLICT));
+        mockMvc.perform(getRequest)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].username", is(user.getUsername())))
+                .andExpect(jsonPath("$[0].password", is(user.getPassword())))
+                .andExpect(jsonPath("$[0].userStatus", is(user.getUserStatus().toString())));
+    }
 
-    UserPostDTO userPostDTO = new UserPostDTO();
-    //userPostDTO.setName("Test User");
-    userPostDTO.setUsername("testUsername");
-    userPostDTO.setPassword("password");
+    // 2. Create user: Status Code 201 CREATED expected
+    @Test
+    public void createUser_validInput_userCreated() throws Exception {
 
-    MockHttpServletRequestBuilder postRequest = post("/users")
-        .contentType(MediaType.APPLICATION_JSON)
-        .content(asJsonString(userPostDTO));
+        user.setUserId(1L);
+        given(userService.createUser(Mockito.any())).willReturn(user);
 
-    mockMvc.perform(postRequest)
-      .andExpect(status().isConflict());
+        // Test with valid input
+        MockHttpServletRequestBuilder postRequestValid = post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(userPostDTO));
 
-  }
+        mockMvc.perform(postRequestValid)
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.userId", is(user.getUserId().intValue())))
+                .andExpect(jsonPath("$.username", is(user.getUsername())))
+                .andExpect(jsonPath("$.password", is(user.getPassword())))
+                .andExpect(jsonPath("$.userStatus", is(user.getUserStatus().toString())));
 
-  // 4. Get user: profile with userID: Status Code 200 OK expected
-  @Test
-  public void getUser_validInput_returnJSONUser() throws Exception {
-      // given
-      User user = new User();
-      user.setUserId(999L);
-      user.setUsername("firstname@lastname");
-      user.setUserStatus(UserStatus.OFFLINE);
-      user.setPassword("password");
-      user.setCreation_date(new Date());
+    }
 
-      // this mocks the UserService -> we define above what the userService should
-      // return when getUser() is called
-      given(userService.getUserById(999L)).willReturn(user);
+    // 3. Create user: new user failed because username already exists: Status Code 409 CONFLICT expected
+    @Test
+    public void createUser_invalidInput() throws Exception {
+        given(userService.createUser(Mockito.any())).willThrow(new ResponseStatusException(HttpStatus.CONFLICT));
 
-      // when
-      MockHttpServletRequestBuilder getRequest = get("/users/999").contentType(MediaType.APPLICATION_JSON);
+        MockHttpServletRequestBuilder postRequest = post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(userPostDTO));
 
-      // then
-      mockMvc.perform(getRequest).andExpect(status().isOk())
-              .andExpect(jsonPath("$.username", is(user.getUsername())))
-              .andExpect(jsonPath("$.userStatus", is(user.getUserStatus().toString())));
-  }
+        mockMvc.perform(postRequest)
+                .andExpect(status().isConflict());
+
+    }
+
+    // 4. Get user: profile with userID: Status Code 200 OK expected
+    @Test
+    public void getUser_validInput_returnJSONUser() throws Exception {
+        // this mocks the UserService -> we define above what the userService should
+        // return when getUser() is called
+        given(userService.getUserById(999L)).willReturn(user);
+
+        // when
+        MockHttpServletRequestBuilder getRequest = get("/users/999").contentType(MediaType.APPLICATION_JSON);
+
+        // then
+        mockMvc.perform(getRequest).andExpect(status().isOk())
+                .andExpect(jsonPath("$.userId", is(user.getUserId().intValue())))
+                .andExpect(jsonPath("$.username", is(user.getUsername())))
+                .andExpect(jsonPath("$.password", is(user.getPassword())))
+                .andExpect(jsonPath("$.userStatus", is(user.getUserStatus().toString())));
+
+    }
 
   // 5. Get user: User with userID was not found: Status Code 404 NOT_FOUND expected
   @Test
@@ -169,18 +154,6 @@ public class UserControllerTest {
   // 6. Update user: Status Code 204 NO_CONTENT expected
   @Test
   public void updateUser_validInput_userUpdated() throws Exception {
-      // given
-        User user = new User();
-        user.setUserId(1L);
-        user.setUsername("testUsername");
-        user.setToken("1");
-        user.setUserStatus(UserStatus.ONLINE);
-        user.setPassword("password");
-        user.setCreation_date(new Date());
-
-        UserPutDTO userPutDTO = new UserPutDTO();
-        userPutDTO.setUsername("testUsername");
-        userPutDTO.setBirthday(new Date(552218400000L));
 
       doNothing().when(userService).updateUser(1L, user);
 
@@ -201,10 +174,6 @@ public class UserControllerTest {
     
     doThrow(new ResponseStatusException(HttpStatus.NOT_FOUND)).when(userService).updateUser(Mockito.anyLong(),Mockito.any());
 
-
-    UserPutDTO userPutDTO = new UserPutDTO();
-        userPutDTO.setUsername("testUsername");
-        userPutDTO.setBirthday(new Date(552218400000L));
 
     MockHttpServletRequestBuilder putRequest = put("/users/1")
                 .contentType(MediaType.APPLICATION_JSON)
--- src/test/java/ch/uzh/ifi/hase/soprafs23/controller/WSGameControllerTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/controller/WSGameControllerTest.java
@@ -1,11 +1,15 @@
 package ch.uzh.ifi.hase.soprafs23.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
-import static org.mockito.BDDMockito.given;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -23,10 +27,12 @@ import org.springframework.web.socket.messaging.WebSocketStompClient;
 import org.springframework.web.socket.sockjs.client.SockJsClient;
 import org.springframework.web.socket.sockjs.client.WebSocketTransport;
 
+
 import ch.uzh.ifi.hase.soprafs23.entity.Game;
 import ch.uzh.ifi.hase.soprafs23.entity.PlayerJoinMessage;
 import ch.uzh.ifi.hase.soprafs23.entity.User;
 import ch.uzh.ifi.hase.soprafs23.service.GameService;
+
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -84,22 +90,34 @@ public class WSGameControllerTest {
 
     }
 
-@Test
-void verifyGameIsReceived() throws Exception {
- 
-  given(gameService.websocketJoin(Mockito.any(), Mockito.any())).willReturn(game);
+    @Test
+    void verifyGameIsReceived() throws Exception {
 
-  MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        // mock the game service to return the game when a user joins
+        given(gameService.websocketJoin(Mockito.any(), Mockito.any())).willReturn(game);
 
-  PlayerJoinMessage playerJoinMessage = new PlayerJoinMessage(1L);
+        // create a WebSocket message converter to convert the player join message to a message that can be sent over STOMP
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
 
-  session.send("/game/join/3", converter.toMessage(playerJoinMessage, null));
+        // create a player join message for the host user
+        PlayerJoinMessage playerJoinMessage = new PlayerJoinMessage(1L);
 
-  Game receivedGame = completableFuture.get(60, TimeUnit.SECONDS);
+        // send the player join message to the server over STOMP
+        session.send("/game/join/3", converter.toMessage(playerJoinMessage, null));
 
-  assertNotNull(receivedGame);
-  
-}
+        // wait for the game to be received over STOMP
+        Game receivedGame = completableFuture.get(60, TimeUnit.SECONDS);
+
+        // assert that the received game is not null and matches the expected game
+        assertNotNull(receivedGame);
+        assertEquals(game.getGameId(), receivedGame.getGameId());
+        assertEquals(game.getHost().getUserId(), receivedGame.getHost().getUserId());
+        assertEquals(game.getGuest().getUserId(), receivedGame.getGuest().getUserId());
+
+        // verify that the game service was called once with the correct arguments
+        verify(gameService, times(1)).websocketJoin(eq(3L), eq(null));
+    }
+
 
 @Test
 void verifyStartGameIsReceived() throws Exception {
