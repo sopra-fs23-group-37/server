dcc88a2d1eb01addcf0022fc68cfa726df0607bd
Carol Ernst <carolbarbara.ernst@uzh.ch>
2023-05-04 18:43:52 +0200

    clean up imports

--- src/test/java/ch/uzh/ifi/hase/soprafs23/service/WebsocketServiceTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/service/WebsocketServiceTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 
