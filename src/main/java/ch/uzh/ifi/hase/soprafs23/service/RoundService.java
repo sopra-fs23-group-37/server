9b3b6e7feaf9d00fc73bfb14ce3f9cc1821f71c4
Carol Ernst <carolbarbara.ernst@uzh.ch>
2023-05-21 11:36:28 +0200

    removed comments

--- src/main/java/ch/uzh/ifi/hase/soprafs23/service/RoundService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/service/RoundService.java
@@ -136,13 +136,10 @@ public class RoundService {
 
         // give any remaining table cards to the player who last grabbed some cards
         Player recipient = round.getLastCardGrab() == Role.HOST ? round.getHost() : round.getGuest();
-        // List<Card> capturedCards = new ArrayList<>();
         for (Card c : round.getTableCards()) {
             recipient.addCardToDiscard(c);
-            // capturedCards.add(c);
         }
 
-        // recipient.setLastCapturedCards(capturedCards);
         round.removeAllCardsFromTable();
 
         // count points of the hands
